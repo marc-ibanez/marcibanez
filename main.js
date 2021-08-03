73cbe9f64078407856918464f3b41f382da1dbf5
Marc Ibañez <84724485+marc-ibanez@users.noreply.github.com>
2021-08-03 12:21:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -167,21 +167,20 @@ themeButton.addEventListener('click', () => {
 })
 
 
-
 // COOKIES
 const cookieBox = document.querySelector(".wrapper"),
-acceptBtn = cookieBox.querySelector("buttons");
+acceptBtn = cookieBox.querySelector("button");
 
 
 acceptBtn.onclick = ()=>{
   //setting cookie for 1 month, after one month it'll be expired automatically
-  document.cookie = "CookieBy=MarcIbañez; max-age="+60*60*24*30;
+  document.cookie = "CookieBy=MarcIbaÃ±ez; max-age="+60*60*24*30;
   if(document.cookie){ //if cookie is set
     cookieBox.classList.add("hide"); //hide cookie box
   }else{ //if cookie not set then alert an error
     alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
   }
 }
-let checkCookie = document.cookie.indexOf("CookieBy=MarcIbañez"); //checking our cookie
+let checkCookie = document.cookie.indexOf("CookieBy=MarcIbaÃ±ez"); //checking our cookie
 //if cookie is set then hide the cookie box else show it
 checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
